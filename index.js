4de93f6122de2c42bf078debaf39950447b5c88d
Travis Surmi <travissurmi@Traviss-MacBook-Pro.local>
2017-06-29 10:13:36 -0700

    Added_dateCreator

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+//if you create your own module, you have to be path specific. You have to
+//go to the exact path it comes from.
+
 var express = require('express');
 var bodyParser = require('body-parser');
+var makeDate = require('./dateMaker');
 var app = express();
+
+console.log(makeDate(10));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
